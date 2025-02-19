f745e9e52aedbf8569eb1400590e1b53771e3a5e
unknown <neilmariano@rocketmail.com>
2025-02-18 23:12:34 -0700

    New C code

---
+++ Hello.c
@@ -0,0 +1,12 @@
+//set Path=C:\MinGW\bin;%PATH%
+
+//https://www.youtube.com/watch?v=9umV9jD6n80
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World! C style!");
+   return 0;
+}
+// gcc source_file.c -o program_name
+// https://www.youtube.com/watch?v=LE9iJ9TpZlU
