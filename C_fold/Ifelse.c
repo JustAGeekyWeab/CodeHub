d245595efab4f1b4104e40579661719672072f1d
unknown <neilmariano@rocketmail.com>
2025-02-22 19:09:23 -0700

    Math, if else, and primary types

---
+++ C_fold/Ifelse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int a = 0;
+    if(a == 0){
+        printf("A is 0\n");
+    }
+    else{
+        printf("A is not 0\n");
+    }
+
+    int b = 1;
+    (b == 1) ? printf("B is 1\n") : printf("B is not 1\n");
+
+
+    return 0;
+}
---
+++ C_fold/PrimaryTypes.c
@@ -0,0 +1,13 @@
+int main(){
+
+    printf("int is an integer variable\n");
+    printf("long is a long integer variable\n");
+    printf("short is a short integer variable\n");
+    printf("signed is a signed variable\n");
+    printf("double is a double-precision variable\n");
+    printf("char is a character variable\n");
+    printf("float is a floating poiont variable\n");
+    printf("unsigned is an unsigned variable\n");
+    printf("void specifies a void return type\n");
+    //https://www.tutorialspoint.com/cprogramming/c_keywords.htm More stuff here
+}
