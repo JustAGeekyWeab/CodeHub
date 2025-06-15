1a7164d145d58adb27d52c9cfc2ffd759849d175
unknown <neilmariano@rocketmail.com>
2025-06-14 23:03:33 -0600

    Send

--- Python/Chess/ChessMain.py
+++ Python/Chess/ChessMain.py
@@ -1,9 +1,9 @@
 import pygame as p
 
-from Chess import ChessEngine 
+import ChessEngine
 
 WIDTH = HEIGHT = 512 # 400 is another option
-DIMENSION = 8 # dimensions of a ches sboard are 8x8
+DIMENSION = 8 # dimensions of a chess board are 8x8
 SQ_SIZE = HEIGHT // DIMENSION 
 MAX_FPS = 15 # for animation
 IMAGES = {}
